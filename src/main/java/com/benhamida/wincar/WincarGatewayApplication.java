5414942bcf534d8676c576b3243f1edfdb9469df
yousseffathi544 <yousseffathi544@gmail.com>
2025-05-17 20:24:41 +0100

    obenhamida change config from .properties to yml

--- src/main/java/com/benhamida/wincar/WincarGatewayApplication.java
+++ src/main/java/com/benhamida/wincar/WincarGatewayApplication.java
@@ -1,17 +1,38 @@
 package com.benhamida.wincar;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.ContextClosedEvent;
 
 @SpringBootApplication
 public class WincarGatewayApplication {
 
+	
+	
+	private static Logger logger = LoggerFactory.getLogger(WincarGatewayApplication.class);
+
+	public WincarGatewayApplication() {
+		super();
+	}
+
+	public static ConfigurableApplicationContext startMicroService(String[] args) {
+		ConfigurableApplicationContext ctx = SpringApplication.run(WincarGatewayApplication.class, args);
+		ctx.addApplicationListener((ContextClosedEvent arg0) -> ctx.close());
+		return ctx;
+	}
+
 	public static void main(String[] args) {
-		SpringApplication.run(WincarGatewayApplication.class, args);
+		ConfigurableApplicationContext ctx = WincarGatewayApplication.startMicroService(args);
+		if (ctx.isActive() && logger.isInfoEnabled()) {
+			logger.info("Micro service [WINCAR-GATEWAY] started correctly");
+		}
 	}
 	
 	/**
@@ -19,11 +40,12 @@ public class WincarGatewayApplication {
 	 * @param rdc
 	 * @param dlp
 	 * @return
-	 */
+	 
 	@Bean
 	DiscoveryClientRouteDefinitionLocator locator(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp) {
 		
 		return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
 	}
+	*/
 
 }
